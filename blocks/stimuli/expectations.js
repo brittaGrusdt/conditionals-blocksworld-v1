d06e7ea2964f6614b3ddfce12da1793784575fd2
bgrusdt <grusdt@uni-potsdam.de>
2020-04-05 17:59:06 +0200

    updated expectations

--- blocks/stimuli/expectations.js
+++ blocks/stimuli/expectations.js
@@ -1,53 +1,52 @@
 let expected_utts = {};
 
-expected_utts["low_low_independent"] = ["Neither A nor C.", "not A", "not C"]
-expected_utts["low_low_a_implies_c"] = ["Neither A nor C.", "not A", "not C",
-  "If A fell, C would fall.(german: falls/sollte A ...)", "If C, A."]
-expected_utts["low_low_a_iff_c"] = ["Neither A nor C.", "not A", "not C",
-  "[If A, C]", "[If C, A]"]
+expected_utts["low_low_independent"] = ["Neither A(left) nor C(right).", "not A(left)", "not C(right)"]
+expected_utts["low_low_a_implies_c"] = ["Neither A(left) nor C(right).", "not A(left)", "not C(right)",
+  "If A(left) fell, C(right) would fall.(german: falls/sollte A(left) ...)", "If C(right), A(left)."]
+expected_utts["low_low_a_iff_c"] = ["Neither A(left) nor C(right).", "not A(left)", "not C(right)",
+  "[If A(left), C(right)]", "[If C(right), A(left)]"]
 
-expected_utts["low_uncertain_independent"] = ["not A", "maybe C"]
-expected_utts["low_uncertain_a_implies_c"] = ["not A", "maybe C",
-  "If A fell, C would fall.(german: falls/sollte A ...)"]
-expected_utts["low_uncertain_a_iff_c"] = ["maybe C",
-  "If C falls, A falls.",  "A may fall because of C"]
+expected_utts["low_uncertain_independent"] = ["not A(left)", "maybe C(right)"]
+expected_utts["low_uncertain_a_implies_c"] = ["not A(left)", "maybe C(right)",
+  "If A(left) fell, C(right) would fall.(german: falls/sollte A(left) ...)"]
+expected_utts["low_uncertain_a_iff_c"] = ["maybe C(right)",
+  "If C(right) falls, A(left) falls.",  "A(left) may fall because of C(right)"]
 
-expected_utts["low_high_independent"] = ["C but not A", "not A", "C", "probably C"]
-expected_utts["low_high_a_implies_c"] = ["C but not A", "not A", "C",
-  "If A falls, C falls.(german: falls/sollte A ...)"]
-expected_utts["low_high_a_iff_c"] = ["C", "C may make A fall", "maybe A", "If A, C."]
+expected_utts["low_high_independent"] = ["C(right) but not A(left)", "not A(left)", "C(right)", "probably C(right)"]
+expected_utts["low_high_a_implies_c"] = ["C(right) but not A(left)", "not A(left)", "C(right)",
+  "If A(left) falls, C(right) falls.(german: falls/sollte A(left) ...)"]
+expected_utts["low_high_a_iff_c"] = ["C(right)", "C(right) may make A(left) fall", "maybe A(left)", "If A(left), C(right)."]
 
-expected_utts["uncertain_low_independent"] = ["maybe A", "probably A", "not C"]
-expected_utts["uncertain_low_a_implies_c"] = ["probably A", "If A, C", "A (may) make(s) C fall",
-  "maybe A", "maybe C"]
-expected_utts["uncertain_low_a_iff_c"] = ["maybe A", "If C, A", "If A, probably C"]
+expected_utts["uncertain_low_independent"] = ["maybe A(left)", "probably A(left)", "not C(right)"]
+expected_utts["uncertain_low_a_implies_c"] = ["probably A(left)", "If A(left), C(right)", "A(left) (may) make(s) C(right) fall",
+  "maybe A(left)", "maybe C(right)"]
+expected_utts["uncertain_low_a_iff_c"] = ["maybe A(left)", "If C(right), A(left)", "If A(left), probably C(right)"]
 
-expected_utts["uncertain_uncertain_independent"] = ["A and C may both", "maybe A", "maybe C"]
-expected_utts["uncertain_uncertain_a_implies_c"] = ["probably A and C", "A and C", "If A, C"]
-expected_utts["uncertain_uncertain_a_iff_c"] = ["maybe A", "maybe C", "If A, C", "If C, A"]
+expected_utts["uncertain_uncertain_independent"] = ["A(left) and C(right) may both", "maybe A(left)", "maybe C(right)"]
+expected_utts["uncertain_uncertain_a_implies_c"] = ["probably A(left) and C(right)", "A(left) and C(right)", "If A(left), C(right)"]
+expected_utts["uncertain_uncertain_a_iff_c"] = ["maybe A(left)", "maybe C(right)", "If A(left), C(right)", "If C(right), A(left)"]
 
-expected_utts["uncertain_high_independent"] = ["maybe A", "C"]
-expected_utts["uncertain_high_a_implies_c"] = ["maybe A", "C", "[If A, C]"]
-expected_utts["uncertain_high_a_iff_c"] = ["maybe A", "C", "A because of C."]
+expected_utts["uncertain_high_independent"] = ["maybe A(left)", "C(right)"]
+expected_utts["uncertain_high_a_implies_c"] = ["maybe A(left)", "C(right)", "[If A(left), C(right)]"]
+expected_utts["uncertain_high_a_iff_c"] = ["maybe A(left)", "C(right)", "A(left) because of C(right)."]
 
-expected_utts["high_low_independent"] = ["A but not C", "A", "not C"]
-expected_utts["high_low_a_implies_c"] = ["C because of A", "A and C", "A", "maybe C"]
-expected_utts["high_low_a_iff_c"] = ["A but not C", "If A, C may fall.", "A may/might make C fall", "If C, A."]
+expected_utts["high_low_independent"] = ["A(left) but not C(right)", "A(left)", "not C(right)"]
+expected_utts["high_low_a_implies_c"] = ["C(right) because of A(left)", "A(left) and C(right)", "A(left)", "maybe C(right)"]
+expected_utts["high_low_a_iff_c"] = ["A(left) but not C(right)", "If A(left), C(right) may fall.", "A(left) may/might make C(right) fall", "If C(right), A(left)."]
 
-expected_utts["high_uncertain_independent"] = ["A", "maybe C", "probably C"]
-expected_utts["high_uncertain_a_implies_c"] = ["probably A and C", "A and C", "[C because of A]"]
-expected_utts["high_uncertain_a_iff_c"] = ["probably A and C", "A and C", "A",
-  "maybe C", "[If C, A]", "[If A, C]"]
+expected_utts["high_uncertain_independent"] = ["A(left)", "maybe C(right)", "probably C(right)"]
+expected_utts["high_uncertain_a_implies_c"] = ["probably A(left) and C(right)", "A(left) and C(right)", "[C(right) because of A(left)]"]
+expected_utts["high_uncertain_a_iff_c"] = ["probably A(left) and C(right)", "A(left) and C(right)", "A(left)",
+  "maybe C(right)", "[If C(right), A(left)]", "[If A(left), C(right)]"]
 
-expected_utts["high_high_independent"] = ["A and C"]
-expected_utts["high_high_a_implies_c"] = ["A and C"]
-expected_utts["high_high_a_iff_c"] = ["A and C", "If A, C", "If C, A"]
+expected_utts["high_high_independent"] = ["A(left) and C(right)"]
+expected_utts["high_high_a_implies_c"] = ["A(left) and C(right)"]
+expected_utts["high_high_a_iff_c"] = ["A(left) and C(right)", "If A(left), C(right)", "If C(right), A(left)"]
 
 getExpectations = function(stimulus){
   let pa = stimulus.meta[0];
   let pc = stimulus.meta[1];
   let rel = stimulus.meta[2];
   let key = [pa, pc, rel].join("_");
-  console.log(expected_utts[key]);
   return expected_utts[key];
 }
