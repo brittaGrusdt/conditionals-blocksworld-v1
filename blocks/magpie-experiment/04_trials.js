7e7a57584e1803fd4baf5c14431a1a7ef3352a24
bgrusdt <grusdt@uni-potsdam.de>
2020-04-23 15:59:23 +0200

    updated QUDs in trials

--- blocks/magpie-experiment/04_trials.js
+++ blocks/magpie-experiment/04_trials.js
@@ -1,7 +1,7 @@
 // In this file you can specify the trial data for your experiment
 var color_vision_test = [
   {
-    QUD: "This is a premaring color vision test",
+    QUD: "Please click on the button with the correct answer.",
     question: "Is the BLUE bock on the right side?",
     picture1: "../stimuli/img/color_vision_test0.jpg",
     picture2: "../stimuli/img/color_vision_test1.jpg",
@@ -11,7 +11,7 @@ var color_vision_test = [
 
   },
   {
-    QUD: "This is a premaring color vision test",
+    QUD: "Please click on the button with the correct answer.",
     question: "Is the GREEN bock on the right side?",
     picture1: "../stimuli/img/color_vision_test1.jpg",
     picture2: "../stimuli/img/color_vision_test0.jpg",
@@ -21,7 +21,7 @@ var color_vision_test = [
 
   },
   {
-    QUD: "This is a premaring color vision test",
+    QUD: "Please click on the button with the correct answer.",
     picture1: "../stimuli/img/color_vision_train0.jpg",
     picture2: "../stimuli/img/color_vision_train1.jpg",
     option1: "no",
@@ -31,7 +31,7 @@ var color_vision_test = [
 
   },
   {
-    QUD: "This is a premaring color vision test",
+    QUD: "Please click on the button with the correct answer.",
     picture1: "../stimuli/img/color_vision_train0.jpg",
     picture2: "../stimuli/img/color_vision_train1.jpg",
     option1: "no",
@@ -46,244 +46,244 @@ var color_vision_test = [
 var slider_rating_trials = [
   {
     picture: "../stimuli/img/group/a_implies_c_ll.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_lu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_lh.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_ul.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_uu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_uh.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_hl.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_hu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_implies_c_hh.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_iff_c_ll.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_iff_c_ul.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_iff_c_hl.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
 
   {
     picture: "../stimuli/img/group/a_iff_c_uu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_iff_c_hu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/a_iff_c_hh.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_ll.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_lu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_lh.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_ul.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_uu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_uh.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_hl.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_hu.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   },
   {
     picture: "../stimuli/img/group/independent_hh.jpg",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2Question.bg,
     question2: id2Question.b,
     question3: id2Question.g,
     question4: id2Question.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   }
 
 ];
@@ -307,13 +307,13 @@ _.map(slider_rating_trials, function (trial) {
 let train_slider_trials = [
   {
     picture: "",
-    QUD: "Rate how likely each of the following statements is!",
+    QUD: "Please indicate how likely you think the represented events will occur!",
     question1: id2QuestionTrain.ac,
     question2: id2QuestionTrain.a,
     question3: id2QuestionTrain.c,
     question4: id2QuestionTrain.none,
     optionLeft: "impossible",
-    optionRight: "certain"
+    optionRight: "certainly"
   }
 ];
 
