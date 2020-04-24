4bbc69f7491ab3d08a9ac164469983834e9addc9
MalinSpaniol <mspaniol@uos.de>
2020-04-24 09:31:44 +0200

    styling of buttons

--- blocks/magpie-experiment/02_custom_functions_utils.js
+++ blocks/magpie-experiment/02_custom_functions_utils.js
@@ -1,4 +1,4 @@
-sliderTexts = function(col1, col2, key1, key2) {
+sliderTexts = function (col1, col2, key1, key2) {
   let id2Text = {};
   id2Text[key1 + key2] = "<b>" + col1 + " will</b> and <b>" + col2 + " will</b> fall.";
   id2Text[key1] = "<b>" + col1 + " will not</b> and <b>" + col2 + " will</b> fall.";
@@ -7,7 +7,7 @@ sliderTexts = function(col1, col2, key1, key2) {
   return id2Text;
 }
 
-sliderIcons = function(cols, keys) {
+sliderIcons = function (cols, keys) {
   let id2Html = {}
   id2Html[keys[0] + keys[1]] = `<div>` +
     `<img src=../stimuli/img/icons/` + cols[0] + `.jpg>` +
@@ -31,7 +31,10 @@ sliderIcons = function(cols, keys) {
   return id2Html;
 }
 
-let block_cols = {test: ['green', 'blue'], train: ['red', 'yellow']}
+let block_cols = {
+  test: ['green', 'blue'],
+  train: ['red', 'yellow']
+}
 
 // let id2Question = sliderTexts(block_cols.test[0], block_cols.test[1], 'b', 'g');
 let id2Question = sliderIcons(block_cols.test, ['b', 'g']);
@@ -40,14 +43,14 @@ let id2QuestionTrain = sliderIcons(block_cols.train, ['a', 'c']);
 // let id2QuestionTrain = sliderTexts(block_cols.train[0], block_cols.train[1], 'a', 'c');
 let text_train_buttons = {
   'ac': block_cols.train[0] + " and " + block_cols.train[1],
-  'a': block_cols.train[0] + " but <b>not</b> "  + block_cols.train[1],
+  'a': block_cols.train[0] + " but <b>not</b> " + block_cols.train[1],
   'c': "<b>Not </b>" + block_cols.train[0] + " but " + block_cols.train[1],
   'none': "<b>Neither </b>" + block_cols.train[0] + " <b>nor</b> " + block_cols.train[1]
 };
 
 // function to randomly order the four utterences, given per trial
 // shuffle_trial_questions
-function shuffleQuestionsAllTrials(questions, slider_rating_trials=[{}]) {
+function shuffleQuestionsAllTrials(questions, slider_rating_trials = [{}]) {
   for (var i = 0; i < slider_rating_trials.length; i++) {
     let utterances = _.shuffle(questions);
     slider_rating_trials[i].question1 = utterances[0];
@@ -58,15 +61,18 @@ function shuffleQuestionsAllTrials(questions, slider_rating_trials=[{}]) {
   return slider_rating_trials;
 }
 
-_htmlSliderQuestion = function(idx_question) {
+_htmlSliderQuestion = function (idx_question) {
   let o = `<q` + idx_question +
     ` class='magpie-view-question grid-question' id ='question` +
     idx_question + `'>`;
   let c = `</q` + idx_question + `>`;
-  return {open: o, close: c};
+  return {
+    open: o,
+    close: c
+  };
 }
 
-_htmlSlider = function(idxSlider, utterance, options) {
+_htmlSlider = function (idxSlider, utterance, options) {
   let sliderID = "slider" + idxSlider
   let responseID = "response" + idxSlider
   let answerID = "answer" + idxSlider
@@ -90,34 +96,38 @@ _htmlSlider = function(idxSlider, utterance, options) {
   return html_question + html_slider
 }
 
-htmlSliderAnswers = function(trial_data) {
+htmlSliderAnswers = function (trial_data) {
   let utterances = [trial_data.question1, trial_data.question2,
     trial_data.question3, trial_data.question4];
   const option1 = trial_data.optionLeft;
   const option2 = trial_data.optionRight;
 
   let html_str = `<div class='magpie-multi-slider-grid' id='answerSliders'>`;
-  _.range(1,5).forEach(function(i){
-    let h = _htmlSlider(i, utterances[i-1], {left: option1, right: option2});
-    html_str += h;
-  });
+  _.range(1, 5)
+    .forEach(function (i) {
+      let h = _htmlSlider(i, utterances[i - 1], {
+        left: option1,
+        right: option2
+      });
+      html_str += h;
+    });
   html_str += `</div>`
   return html_str;
 }
 
-htmlButtonAnswers = function() {
+htmlButtonAnswers = function () {
   return `<bttns id=TrainButtons class=buttonContainer>
-    <button id="ac" class=unselected>` + id2QuestionTrain.ac + `</button>
+    <button id="ac" class="unselected styled-button">` + id2QuestionTrain.ac + `</button>
     <div class="divider"/>
-    <button id="a" class=unselected>` + id2QuestionTrain.a + `</button>
+    <button id="a" class="unselected styled-button">` + id2QuestionTrain.a + `</button>
     <div class="divider"/>
-    <button id="c" class=unselected>` + id2QuestionTrain.c + `</button>
+    <button id="c" class="unselected styled-button">` + id2QuestionTrain.c + `</button>
     <div class="divider"/>
-    <button id="none" class=unselected>` + id2QuestionTrain.none + `</button>
+    <button id="none" class="unselected styled-button">` + id2QuestionTrain.none + `</button>
   </bttns>`;
 }
 
-htmlRunNextButtons = function() {
+htmlRunNextButtons = function () {
   let htmlBttns =
     `<div id=parentRunNext class=magpie-buttons-grid>
       <run>
